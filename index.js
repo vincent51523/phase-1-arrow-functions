2a6b43c9b99adcdc8bb8694af8f38a23a5795e05
bin <vincent418@icloud.com>
2021-04-22 10:36:04 -0500

    commit tree d8bf7149bf58cb437baffcecefb92513136ac4d7

---
+++ index.js
@@ -0,0 +1,12 @@
+// const divide = (x) => 2000 / 100
+// function divide(）{
+// return 2000 / 100 ;
+// }
+const divide = function() {
+    return 2000 / 100 ;
+}
+
+const square = (x) => x * x ;
+const add = (x1 , x2) => {
+    return x1 + x2 ;
+}
